fcb001424a8bbdcd82dd65b57d2e9607ef60f074
hiroki nakahashi <hirofighter@gmail.com>
2020-04-09 08:03:55 +0900

    model実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_012759) do
+ActiveRecord::Schema.define(version: 2020_04_08_044812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2020_04_06_012759) do
   create_table "messages", force: :cascade do |t|
     t.string "name"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.string "detail"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
